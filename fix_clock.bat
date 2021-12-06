01bd733bf224dc9e9832adda9cda46fad22eec3e
Nitzan Bresler <80216549+Stormageddon37@users.noreply.github.com>
2021-12-07 00:31:42 +0200

    minor fixes, removed unreachable code

--- fix_clock.bat
+++ fix_clock.bat
@@ -1,6 +1,9 @@
 @echo off
+cls
 SET MyServer=www.google.com
+
 ECHO Testing computer's connection to the internet, pinging %MyServer%
+
 :START
 %SystemRoot%\system32\ping.exe -n 1 %MyServer% >nul
 if errorlevel 1 GOTO OFFLINE
@@ -8,12 +11,10 @@ if errorlevel 1 GOTO OFFLINE
 ECHO This computer is online!
 net start w32time
 w32tm /resync
-PAUSE
+ECHO Clock syncing completed!
 GOTO :EOF
 
 :OFFLINE
 ECHO This computer is not online. Trying again...
 TIMEOUT /T 5 >NUL
 GOTO :START
-PAUSE
-GOTO :EOF
