afaff93e57eddcbfb67232bb0c00238221726253
Nitzan Bresler <80216549+Stormageddon37@users.noreply.github.com>
2021-12-07 00:20:35 +0200

    script now checks for an internet connection

--- fix_clock.bat
+++ fix_clock.bat
@@ -1,6 +1,19 @@
 @echo off
-color a
-cd c:/
-cls
-for /l %%x in (1, 1, 10) do net start w32time
+SET MyServer=www.google.com
+ECHO Testing computer's connection to the internet, pinging %MyServer%
+:START
+%SystemRoot%\system32\ping.exe -n 1 %MyServer% >nul
+if errorlevel 1 GOTO OFFLINE
+
+ECHO This computer is online!
+net start w32time
 w32tm /resync
+PAUSE
+GOTO :EOF
+
+:OFFLINE
+ECHO This computer is not online. Trying again...
+TIMEOUT /T 5 >NUL
+GOTO :START
+PAUSE
+GOTO :EOF
